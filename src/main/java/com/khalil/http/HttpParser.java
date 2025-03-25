92d251fbdaec41576611f0f3cb1ed45af6db1ca6
flannelmonke <khalilanaji@gmail.com>
2025-03-25 12:31:45 -0400

    Add HttpParser class for HTTP request parsing

---
+++ src/main/java/com/khalil/http/HttpParser.java
@@ -0,0 +1,15 @@
+package com.khalil.http;
+
+import java.io.InputStream;
+import java.util.logging.Logger;
+
+public class HttpParser {
+
+    private final static Logger LOGGER = Logger.getLogger(HttpParser.class.getName());
+
+    public void parseHttpRequest(InputStream inputStream) {
+    }
+
+    public HttpParser() {
+    }
+}
